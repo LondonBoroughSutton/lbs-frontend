71d8f9947a11a8da1838f4af83e0188aab43cb2c
benwatsonuk <hello@benwatson.eu>
2021-08-16 14:54:41 +0100

    Hover states (basic) added

--- src/LBS/components/header/header.js
+++ src/LBS/components/header/header.js
@@ -1,6 +1,18 @@
 function Header ($module) {
     this.$module = $module
     console.log('Just a test to see if this header component is pulled in')
+    let menuItems = document.querySelectorAll('li.has-submenu');
+    Array.prototype.forEach.call(menuItems, function(el, i){
+        el.addEventListener("mouseover", function(event){
+            this.className = "has-submenu open";
+            clearTimeout(timer);
+        });
+        el.addEventListener("mouseout", function(event){
+            timer = setTimeout(function(event){
+                document.querySelector(".has-submenu.open").className = "has-submenu";
+            }, 1000);
+        });
+    });
 }
 
 export default Header
