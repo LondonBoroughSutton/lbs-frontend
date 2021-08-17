2711a9c4a801188686f2e114ae4e892bcd4ecf9a
benwatsonuk <hello@benwatson.eu>
2021-08-17 09:09:24 +0100

    Started to add 'disclosure menu' - checking in changes to work on a 'fly out' menu instead (seems a better fit)

--- src/LBS/all.js
+++ src/LBS/all.js
@@ -12,19 +12,31 @@ function initAll (options) {
     // Defaults to the entire document if nothing is set.
     var scope = typeof options.scope !== 'undefined' ? options.scope : document
 
-    var $tabs = scope.querySelectorAll('[data-module="govuk-tabs"]')
-    nodeListForEach($tabs, function ($tabs) {
-        new Tabs($tabs).init()
-    })
+    // var $tabs = scope.querySelectorAll('[data-module="govuk-tabs"]')
+    // nodeListForEach($tabs, function ($tabs) {
+    //     new Tabs($tabs).init()
+    // })
 
     var $example = scope.querySelectorAll('.lbs_example')
     nodeListForEach($example, function ($example) {
         new Example($example) // Don't need to init
     })
 
-    var $header = scope.querySelectorAll('.lbs_header')
-    nodeListForEach($header, function ($header) {
-        new Header($header) // Don't need to init
+    var $headers = scope.querySelectorAll('.lbs-header')
+    nodeListForEach($headers, function ($header) {
+        let menu = false
+        if (document.getElementById('lbs-header__navigation')) {
+            menu = document.getElementById('lbs-header__navigation')
+        }
+        new Header($header, menu).init() // Don't need to init
+        // new Header($header) // Don't need to init
+        // if (document.getElementById('lbs-header__navigation2')) {
+        //     console.log('1233')
+        //     // new Header($header).Menubar(document.getElementById('lbs-header__navigation2')).init();
+        //     let menu = new Header($header);
+        //     menu.init()
+        //
+        // }
     })
 }
 
@@ -32,6 +44,5 @@ export {
     initAll,
     Example,
     Header,
-    Tabs,
     Search
 }
