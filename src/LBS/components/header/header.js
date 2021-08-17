a0c8c8f473f5b6c41600f05f42c55136fad566dc
benwatsonuk <hello@benwatson.eu>
2021-08-17 13:27:06 +0100

    JS implementation - flyout menu as described in 'https://www.w3.org/WAI/tutorials/menus/flyout/#flyoutnavmousefixed'

--- src/LBS/components/header/header.js
+++ src/LBS/components/header/header.js
@@ -1,20 +1,38 @@
 function Header($module) {
     this.$module = $module
-    console.log(123)
-    let menuItems = document.querySelectorAll('li.has-submenu');
-    console.log(menuItems)
-    Array.prototype.forEach.call(menuItems,function(el, i){
-        el.querySelector('a').addEventListener("click",  function(event){
-            if (this.parentNode.className == "has-submenu") {
-                this.parentNode.className = "has-submenu open";
+    let menuItems = document.querySelectorAll('li[data-has-submenu]');
+    Array.prototype.forEach.call(menuItems, function (el, i) {
+        el.querySelector('a').addEventListener("click", function (event) {
+            if (this.parentNode.getAttribute('data-has-submenu') === "true" && this.parentNode.getAttribute('data-submenu-open') !== "true") {
+                this.parentNode.setAttribute("data-submenu-open", true);
                 this.setAttribute('aria-expanded', "true");
             } else {
-                this.parentNode.className = "has-submenu";
+                this.parentNode.setAttribute("data-submenu-open", false);
                 this.setAttribute('aria-expanded', "false");
             }
             event.preventDefault();
             return false;
         });
+        let checkIfMenuIsOpen = null
+        let links = el.querySelectorAll('a');
+        Array.prototype.forEach.call(links, function (el, i) {
+            el.addEventListener("focus", function () {
+                if (checkIfMenuIsOpen) {
+                    clearTimeout(checkIfMenuIsOpen);
+                    checkIfMenuIsOpen = null;
+                }
+            });
+            el.addEventListener("blur", function (event) {
+                checkIfMenuIsOpen = setTimeout(function () {
+                    let opennav = document.querySelector("li[data-submenu-open='true']")
+                    if (opennav) {
+                        opennav.setAttribute("data-submenu-open", false);
+                        opennav.querySelector('a').setAttribute('aria-expanded', "false");
+                    }
+                }, 10)
+            });
+        });
     });
 }
+
 export default Header
