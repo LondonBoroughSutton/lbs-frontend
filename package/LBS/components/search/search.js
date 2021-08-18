c004960cc0f9250f5cd7c164ac3eddff00b17001
benwatsonuk <hello@benwatson.eu>
2021-08-18 16:25:29 +0100

    Package Build tests passing and package added to Git

---
+++ package/LBS/components/search/search.js
@@ -0,0 +1,14 @@
+(function (global, factory) {
+	typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+	typeof define === 'function' && define.amd ? define('LBSFrontend', factory) :
+	(global.LBSFrontend = factory());
+}(this, (function () { 'use strict';
+
+function Search ($module) {
+    this.$module = $module;
+    console.log('Just a test to see if this search component is pulled in');
+}
+
+return Search;
+
+})));
