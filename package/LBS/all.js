2398b01a1203d1c413f85078c13d8a186e5ec899
benwatsonuk <hello@benwatson.eu>
2021-09-10 12:04:38 +0100

    V0.0.11

--- package/LBS/all.js
+++ package/LBS/all.js
@@ -18,6 +18,52 @@ function nodeListForEach (nodes, callback) {
   }
 }
 
+function Card ($module) {
+  this.$module = $module;
+}
+
+/**
+ * Initialise header
+ *
+ * Check for the presence of cards - if any are
+ * missing then there's nothing to do so return early.
+ */
+Card.prototype.init = function () {
+  if (!this.$module) {
+    return
+  }
+  if (this.$module.classList.contains('lbs-card--clickable')) {
+    this.handleClickable();
+  }
+  if (this.$module.querySelector('.js__is-hidden')) {
+    this.showAllItems();
+  }
+};
+
+Card.prototype.handleClickable = function () {
+  if (this.$module.querySelector('a') !== null) {
+    this.$module.addEventListener('click', () => {
+      this.$module.querySelector('a').click();
+    });
+  }
+};
+
+Card.prototype.showAllItems = function () {
+  // todo - consider moving focus to previously hidden items on toggle
+  const module = this.$module;
+  const itemCount = this.$module.querySelectorAll('.js__is-hidden').length;
+  const showMoreHtml = document.createElement('a');
+  showMoreHtml.innerText = 'Show more items (' + itemCount + ')';
+  showMoreHtml.setAttribute('class', 'show-more-link');
+  showMoreHtml.setAttribute('href', '#');
+  showMoreHtml.addEventListener('click', function (e) {
+    module.querySelector('ul').classList.add('show-hidden');
+    module.removeChild(this);
+    e.preventDefault();
+  });
+  this.$module.append(showMoreHtml);
+};
+
 function Header ($module) {
   this.$module = $module;
   this.$searchButton = $module && $module.querySelector('#lbs-header__mobile__search-btn');
@@ -145,9 +191,15 @@ function initAll (options) {
   nodeListForEach($headers, function ($header) {
     new Header($header).init();
   });
+
+  var $cards = scope.querySelectorAll('.lbs-card');
+  nodeListForEach($cards, function ($card) {
+    new Card($card).init();
+  });
 }
 
 exports.initAll = initAll;
+exports.Card = Card;
 exports.Header = Header;
 exports.Search = Search;
 
