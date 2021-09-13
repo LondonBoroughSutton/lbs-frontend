294da33c807e66ef512b7e6bc4b7da89659862f1
benwatsonuk <hello@benwatson.eu>
2021-09-13 17:09:17 +0100

    Added some simple tests

---
+++ src/LBS/components/page-group-list/template.test.js
@@ -0,0 +1,33 @@
+/**
+ * @jest-environment jsdom
+ */
+/* eslint-env jest */
+
+const axe = require('../../../../lib/axe-helper')
+
+const { render, getExamples, htmlWithClassName } = require('../../../../lib/jest-helpers')
+
+const examples = getExamples('page-group-list')
+
+// const WORD_BOUNDARY = '\\b'
+// const WHITESPACE = '\\s'
+
+describe('Page Group List', () => {
+  describe('default example', () => {
+    it('passes accessibility tests', async () => {
+      const $ = render('page-group-list', examples.default)
+
+      const results = await axe($.html())
+      expect(results).toHaveNoViolations()
+    })
+
+    it('renders with expected child elements', () => {
+      const $ = render('page-group-list', examples.default)
+
+      const title = $('.lbs-page-group-list .lbs-page-group-list__title')
+      const body = $('.lbs-page-group-list ol')
+      expect(title.length).toBeTruthy()
+      expect(body.length).toBeTruthy()
+    })
+  })
+})
