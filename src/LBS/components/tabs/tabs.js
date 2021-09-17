8ff8abf724282f2a94f0f0e0e5baef17b8988fa2
benwatsonuk <hello@benwatson.eu>
2021-09-17 10:56:35 +0100

    Tidy up

--- src/LBS/components/tabs/tabs.js
+++ src/LBS/components/tabs/tabs.js
@@ -10,18 +10,14 @@ function Tabs ($module) {
 /**
  * Initialise tabs
  *
- * Check for the presence of cards - if any are
+ * Check for the presence of tabs - if any are
  * missing then there's nothing to do so return early.
  */
 Tabs.prototype.init = function () {
   if (!this.$module) {
     return
   }
-
-  console.log('Hello')
   this.setup()
-
-  // this.setHeight()
 }
 
 Tabs.prototype.setup = function () {
@@ -49,38 +45,22 @@ Tabs.prototype.setup = function () {
     // Handle events
     $tab.addEventListener('click', $tab.boundTabClick, true)
     // $tab.addEventListener('keydown', $tab.boundTabKeydown, true)
-
-    // Remove old active panels
-    // this.hideTab($tab)
   }.bind(this))
 
 }
 
-// Tabs.prototype.handleClickable = function () {
-//   if (this.$module.querySelector('a') !== null) {
-//     this.$module.addEventListener('click', () => {
-//       this.$module.querySelector('a').click()
-//     })
-//   }
-// }
-
 Tabs.prototype.setAttributes = function ($tab) {
-  // console.log($tab)
   // set tab attributes
   let panelId = 1
   $tab.setAttribute('id', 'tab_' + panelId)
   $tab.setAttribute('role', 'tab')
   $tab.setAttribute('aria-controls', panelId)
   $tab.setAttribute('aria-selected', 'false')
-  // $tab.setAttribute('tabindex', '-1')
 
   // set panel attributes
-  console.log($tab)
   let $panel = this.getPanel($tab)
-  // console.log($panel)
   $panel.setAttribute('role', 'tabpanel')
   $panel.setAttribute('aria-labelledby', $tab.id)
-  // $panel.classList.add(this.activePanelClass)
 }
 
 Tabs.prototype.unsetAttributes = function ($tab) {
@@ -92,16 +72,13 @@ Tabs.prototype.unsetAttributes = function ($tab) {
   $tab.removeAttribute('tabindex')
 
   // unset panel attributes
-  console.log($tab)
   let $panel = this.getPanel($tab)
   $panel.removeAttribute('role')
   $panel.removeAttribute('aria-labelledby')
-  // $panel.classList.remove(this.activePanelClass)
 }
 
 Tabs.prototype.hideTab = function ($tab) {
   const theTarget = this.$module.querySelector('.lbs-tabs__list-item a[data-lbs-tab-id="' + $tab.getAttribute('data-lbs-tab-id') + '"]')
-  // console.log(theTarget)
   this.unhighlightTab(theTarget)
   this.hidePanel($tab)
 }
@@ -120,49 +97,37 @@ Tabs.prototype.onTabClick = function (e) {
   e.preventDefault()
   let $newTab = e.target
   let $currentTab = this.getCurrentTab()
-  console.log('C: ' + $currentTab)
-  console.log('N: ' + $newTab)
   this.hideTab($currentTab)
   this.showTab($newTab)
-  // this.createHistoryEntry($newTab)
 }
 
 Tabs.prototype.getCurrentTab = function () {
-  console.log('CF')
   return this.$module.querySelector('.' + this.activeTabClass + ' .lbs-tabs__tab')
 }
 
 Tabs.prototype.getPanel = function ($tab) {
-  console.log('GetPanel: ' + $tab)
-  const $panel = this.$module.querySelector('.lbs-tabs__content__item[data-lbs-tab-id="' + $tab.getAttribute('data-lbs-tab-id') + '"]')
-  console.log($panel)
-  return $panel
+  return this.$module.querySelector('.lbs-tabs__content__item[data-lbs-tab-id="' + $tab.getAttribute('data-lbs-tab-id') + '"]')
 }
 
 Tabs.prototype.showPanel = function ($tab) {
-  console.log('ShowPanel: ' + $tab)
   let $panel = this.getPanel($tab)
   $panel.classList.add(this.activePanelClass)
 }
 
 Tabs.prototype.hidePanel = function ($tab) {
-  console.log('HidePanel: ' +   $tab)
   let $panel = this.getPanel($tab)
   $panel.classList.remove(this.activePanelClass)
 }
 
 Tabs.prototype.unhighlightTab = function ($tab) {
-  // console.log($tab)
   $tab.setAttribute('aria-selected', 'false')
   $tab.parentNode.classList.remove(this.activeTabClass)
 }
 
 Tabs.prototype.highlightTab = function ($tab) {
-  console.log('HT: ' + $tab)
   $tab.setAttribute('aria-selected', 'true')
   $tab.parentNode.classList.add(this.activeTabClass)
   $tab.blur()
-  // $tab.setAttribute('tabindex', '0')
 }
 
 export default Tabs
