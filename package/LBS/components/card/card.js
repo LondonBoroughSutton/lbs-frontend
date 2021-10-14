25ac231a303e45719ccd59a73cc31e4bf82a8b05
benwatsonuk <hello@benwatson.eu>
2021-10-14 19:41:20 +0100

    Updated cardheight function/styles

--- package/LBS/components/card/card.js
+++ package/LBS/components/card/card.js
@@ -21,8 +21,12 @@ function ShowMore ($module) {
 }
 
 ShowMore.prototype.init = function () {
-  this.hideItems();
-  if (this.$module.getAttribute('data-show-more')) {
+  const count = this.$module.getAttribute('data-show-count') || 6; // Roadmap item - add data item to dictate how many items to show
+  console.log(parseInt(this.$module.getAttribute('data-show-count')));
+  console.log(count);
+  this.hideItems(count);
+  if (this.$module.getAttribute('data-show-more') && count !== '0') {
+    console.log(1);
     this.addCallToAction();
     if (this.$module.getAttribute('data-show-more-type')) {
       if (this.$module.getAttribute('data-show-more-position')) {
@@ -35,16 +39,16 @@ ShowMore.prototype.init = function () {
   }
 };
 
-ShowMore.prototype.hideItems = function () {
-  const count = parseInt(this.$module.getAttribute('data-show-count')) || 6; // Roadmap item - add data item to dictate how many items to show
+ShowMore.prototype.hideItems = function (count) {
   this.$module.querySelectorAll('.lbs-card:not(.lbs-card--popular-item)').forEach((x, index) => {
-    if (index >= count) {
+    if (index >= count && count != 0) {
       x.parentNode.classList.add('js__is-hidden');
     }
   });
 };
 
 ShowMore.prototype.addCallToAction = function () {
+  console.log('hit');
   const module = this.$module;
   const itemCount = this.$module.querySelectorAll('.js__is-hidden').length;
   const showMoreHtml = document.createElement('a');
@@ -117,7 +121,6 @@ Card.prototype.init = function () {
   if (this.$module.querySelector('.js__is-hidden')) {
     this.showAllItems();
   }
-  new ShowMore(this.$module).init();
 };
 
 Card.prototype.handleClickable = function () {
@@ -160,10 +163,11 @@ Cards.prototype.init = function () {
   } else {
     this.setupCardWrapper();
   }
+  new ShowMore(this.$module).init();
 };
 
 Cards.prototype.setupCardWrapper = function () {
-  console.log('Card setup');
+
 };
 
 Cards.prototype.setupResponsiveChecks = function () {
@@ -175,7 +179,7 @@ Cards.prototype.setupResponsiveChecks = function () {
 Cards.prototype.checkMode = function () {
   if (this.mql.matches) {
     this.setHeight();
-    new ShowMore(this.$module).init();
+    this.setupCardWrapper();
   } else {
     this.teardownCards();
   }
@@ -193,7 +197,7 @@ Cards.prototype.setHeight = function () {
   document.querySelectorAll('.lbs-card').forEach(card => {
     if (card.clientHeight > tallestCard) {
       const cs = window.getComputedStyle(card);
-      tallestCard = card.offsetHeight - (parseFloat(cs.paddingBottom));
+      tallestCard = card.clientHeight - (parseFloat(cs.paddingBottom));
     }
   });
   document.querySelectorAll('.lbs-card:not(.lbs-card--popular-item)').forEach(x => {
