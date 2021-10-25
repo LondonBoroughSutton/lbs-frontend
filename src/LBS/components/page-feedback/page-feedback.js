122e84090375b05c6d0d38982f8c435fda4cf23a
benwatsonuk <hello@benwatson.eu>
2021-10-25 16:10:26 +0100

    JS updated

--- src/LBS/components/page-feedback/page-feedback.js
+++ src/LBS/components/page-feedback/page-feedback.js
@@ -1,5 +1,9 @@
 function PageFeedback ($module) {
   this.$module = $module
+  this.jshiddenClass = 'js_is-hidden'
+  this.prompt = 'lbs-page-feedback__prompt'
+  this.feedbackForm = 'lbs-page-feedback__form'
+  this.formToggleButton = 'lbs-page-feedback__form'
 }
 
 /**
@@ -13,6 +17,34 @@ PageFeedback.prototype.init = function () {
     return
   }
   console.log('Page Feedback')
+  console.log(this.formToggleButton)
+  this.setInitialAriaAttributes()
+  this.$module.querySelector('[aria-controls="lbs-page-feedback"]').addEventListener("click", function(t) {
+    console.log('Clicked')
+    t.preventDefault();
+    // let e = t.target;
+    // this.toggleForm(e.getAttribute("aria-controls"))
+    // this.updateAriaAttributes(e)
+  })
+}
+
+PageFeedback.prototype.setInitialAriaAttributes = function() {
+  console.log('set values')
+  document.getElementById(this.feedbackForm).setAttribute('aria-hidden', true)
+}
+
+PageFeedback.prototype.updateAriaAttributes = function(t) {
+  console.log('updateAriaAttributes')
+  // t.setAttribute("aria-expanded", !0);
+  // var e = t.getAttribute("aria-controls");
+  // document.querySelector("#" + e).setAttribute("aria-hidden", !1)
+}
+
+PageFeedback.prototype.toggleForm = function() {
+  console.log('toggle form')
+  this.prompt.classList.toggle(this.jshiddenClass)
+  this.feedbackForm.classList.toggle(this.jshiddenClass)
+  this.updateAriaAttributes()
 }
 
 export default PageFeedback
