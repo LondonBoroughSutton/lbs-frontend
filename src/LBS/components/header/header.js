35e49852ade582497c4d02487e22f047b8c3e169
benwatsonuk <hello@benwatson.eu>
2021-12-07 09:51:25 +0000

    Progress

--- src/LBS/components/header/header.js
+++ src/LBS/components/header/header.js
@@ -1,14 +1,14 @@
-function Header ($module) {
-  this.$module = $module
-  this.$searchButton = $module && $module.querySelector('#lbs-header__mobile__search-btn')
-  this.$menuButton = $module && $module.querySelector('#lbs-header__mobile__menu-btn')
-  this.$menu = this.$menuButton && $module.querySelector(
-    '#' + this.$menuButton.getAttribute('aria-controls')
-  )
-  this.$search = this.$searchButton && $module.querySelector(
-    '#' + this.$searchButton.getAttribute('aria-controls')
-  )
-}
+// function Header ($module) {
+//   this.$module = $module
+//   this.$searchButton = $module && $module.querySelector('#lbs-header__mobile__search-btn')
+//   this.$menuButton = $module && $module.querySelector('#lbs-header__mobile__menu-btn')
+//   this.$menu = this.$menuButton && $module.querySelector(
+//     '#' + this.$menuButton.getAttribute('aria-controls')
+//   )
+//   this.$search = this.$searchButton && $module.querySelector(
+//     '#' + this.$searchButton.getAttribute('aria-controls')
+//   )
+// }
 
 /**
  * Initialise header
@@ -54,7 +54,7 @@ Header.prototype.init = function () {
       })
       el.addEventListener('blur', function (event) {
         checkIfMenuIsOpen = setTimeout(function () {
-          const opennav = document.querySelector("li[data-submenu-open='true']")
+          const opennav = document.querySelector('li[data-submenu-open=\'true\']')
           if (opennav) {
             opennav.setAttribute('data-submenu-open', false)
             opennav.querySelector('a').setAttribute('aria-expanded', 'false')
@@ -107,6 +107,92 @@ Header.prototype.handleSearchButtonClick = function () {
   if (this.$menu.classList.contains('active')) {
     this.syncState(this.$menuButton, this.$menu.classList.toggle('active'))
   }
+}
+
+// New bits
+
+function Header ($module) {
+    this.$module = $module,
+    this.$navigationToggle = this.$module.querySelector('#super-navigation-menu-toggle'),
+    this.$navigationMenu = this.$module.querySelector('#super-navigation-menu'),
+    this.$searchToggle = this.$module.querySelector('#super-search-menu-toggle'),
+    this.$searchMenu = this.$module.querySelector('#super-search-menu'),
+    this.$buttons = this.$module.querySelectorAll('button[aria-controls][data-toggle-mobile-group][data-toggle-desktop-group]'),
+    this.hiddenButtons = this.$module.querySelectorAll('button[hidden]'),
+    this.lastWindowSize = null
+}
+
+var i = {
+    breakpoint: {
+      desktop: 769
+    },
+    label: {
+      hide: 'data-text-for-hide',
+      show: 'data-text-for-show'
+    }
+  },
+  n = function (e, t) {
+    var n = e.getAttribute(i.label[t])
+    n && e.setAttribute('aria-label', n)
+  },
+  u = function (e, t) {
+    e.setAttribute('aria-expanded', !1), e.classList.remove('gem-c-layout-super-navigation-header__open-button'), t.setAttribute('hidden', 'hidden'), n(e, 'show')
+  },
+  o = function (e, t) {
+    e.setAttribute('aria-expanded', !0), e.classList.add('gem-c-layout-super-navigation-header__open-button'), t.removeAttribute('hidden'), n(e, 'hide')
+  },
+  l = function (e, t) {
+    var n = 'true' === e.getAttribute('aria-expanded'),
+      i = e.getAttribute('data-tracking-key')
+    n ? u(e, t) : o(e, t), window.GOVUK.analytics && window.GOVUK.analytics.trackEvent && i && window.GOVUK.analytics.trackEvent('headerClicked', i + (n ? 'Closed' : 'Opened'), {
+      label: 'none'
+    })
+  },
+  d = function (e, t) {
+    return e.tagName.toLowerCase() === t.toLowerCase() ? e : d(e.parentNode, t)
+  },
+  r = function (e, t) {
+    if (null === e) return null
+    if (1 === e.nodeType && e.tagName.toLowerCase() === t.toLowerCase()) return e
+    var n = e.previousElementSibling || e.previousSibling
+    return r(n, t)
+  },
+  a = function (e) {
+    return 0 < e.querySelectorAll('button[aria-expanded="true"]').length
+  },
+  s = function () {
+    return document.documentElement.clientWidth >= i.breakpoint.desktop ? 'desktop' : 'mobile'
+  }
+t.prototype.windowSize = s, t.prototype.updateStates = function () {
+  if ('mobile' === this.windowSize() && 'mobile' !== this.lastWindowSize && (this.$navigationToggle.removeAttribute('hidden'), a(this.$navigationMenu) || u(this.$navigationToggle, this.$navigationMenu), this.$module.style.marginBottom = '0'), 'desktop' === this.windowSize() && 'desktop' !== this.lastWindowSize && (this.$navigationToggle.setAttribute('hidden', 'hidden'), this.$navigationMenu.removeAttribute('hidden')), 'desktop' === s()) {
+    var e = this.$module.querySelector('[aria-expanded="true"][data-toggle-desktop-group="top"]'),
+      t = e ? this.$module.querySelector('#' + e.getAttribute('aria-controls')) : null,
+      n = t ? t.offsetHeight : 0
+    this.$module.style.marginBottom = n + 'px'
+  }
+  this.lastWindowSize = this.windowSize()
+}, t.prototype.buttonHandler = function (e) {
+  for (var t = d(e.target, 'button'), n = this.$module.querySelector('#' + t.getAttribute('aria-controls')), i = 'data-toggle-' + this.windowSize() + '-group', o = t.getAttribute(i), r = this.$module.querySelectorAll('[' + i + '="' + o + '"]'), a = 0; a < r.length; a++) {
+    var s = r[a]
+    if (s !== t) {
+      var c = this.$module.querySelector('#' + s.getAttribute('aria-controls'))
+      u(s, c)
+    }
+  }
+  l(t, n), 'desktop' === this.windowSize() && (this.$module.style.marginBottom = n.offsetHeight + 'px')
+}, t.prototype.init = function () {
+  for (var e = 0; e < this.$buttons.length; e++) {
+    this.$buttons[e].addEventListener('click', this.buttonHandler.bind(this), !0)
+  }
+  for (var t = 0; t < this.hiddenButtons.length; t++) {
+    var n = this.hiddenButtons[t]
+    n.removeAttribute('hidden')
+    var i = r(n, 'a')
+    i && i.setAttribute('hidden', 'hidden')
+  }
+  this.$module.querySelector('.gem-c-layout-super-navigation-header__search-item-link').setAttribute('hidden', 'hidden'), u(this.$searchToggle, this.$searchMenu), this.updateStates(), this.lastWindowSize = this.windowSize(), window.addEventListener('resize', this.updateStates.bind(this), {
+    passive: !0
+  }), this.$module.classList.add('js-module-initialised')
 }
 
 export default Header
---
+++ src/LBS/components/header2/template.test.js
@@ -0,0 +1,95 @@
+/**
+ * @jest-environment jsdom
+ */
+/* eslint-env jest */
+
+const axe = require('../../../../lib/axe-helper')
+
+const { render, getExamples, htmlWithClassName } = require('../../../../lib/jest-helpers')
+
+const examples = getExamples('header')
+
+// const WORD_BOUNDARY = '\\b'
+// const WHITESPACE = '\\s'
+
+describe('Header', () => {
+  describe('default example', () => {
+    it('passes accessibility tests', async () => {
+      const $ = render('header', examples.default)
+
+      const results = await axe($.html())
+      expect(results).toHaveNoViolations()
+    })
+
+    it('input renders with banner role', () => {
+      const $ = render('header', examples.default)
+
+      const $component = $('.lbs-header')
+      expect($component.attr('role')).toEqual('banner')
+    })
+
+    it('contains logo', () => {
+      const $ = render('header', examples.default)
+
+      const $component = $('.lbs-header .lbs-header__logo svg')
+      expect($component.length).toBeTruthy()
+    })
+
+    it('contains Sutton.gov.uk for assistive technologies', () => {
+      const $ = render('header', examples.default)
+
+      const $component = $('.lbs-header .lbs-header__logo span')
+      expect($component.text()).toContain('Sutton.gov.uk')
+    })
+  })
+
+  describe('when it is instructed to omit items', () => {
+    it('renders without the search', () => {
+      const $ = render('header', examples['with no search'])
+
+      const $component = $('.lbs-header #lbs-header__search')
+      expect($component.length).not.toBeTruthy()
+      expect(htmlWithClassName($, '.lbs-header')).toMatchSnapshot()
+    })
+
+    it('renders without the search but with the nav', () => {
+      const $ = render('header', examples['with no search'])
+
+      const $component = $('.lbs-header #lbs-header__search')
+      expect($component.length).not.toBeTruthy()
+      const $component2 = $('.lbs-header #lbs-header__navigation_container')
+      expect($component2.length).toBeTruthy()
+      expect(htmlWithClassName($, '.lbs-header')).toMatchSnapshot()
+    })
+  })
+
+  describe('macro parameters', () => {
+    it('renders without the nav', () => {
+      const $ = render('header', examples['with no navigation'])
+
+      const $component = $('.lbs-header > #lbs-header__navigation_container')
+      expect($component.length).not.toBeTruthy()
+      expect(htmlWithClassName($, '.lbs-header')).toMatchSnapshot()
+    })
+
+    it('renders without the nav but with the search', () => {
+      const $ = render('header', examples['with no navigation'])
+
+      const $component = $('.lbs-header #lbs-header__search')
+      expect($component.length).toBeTruthy()
+      const $component2 = $('.lbs-header > #lbs-header__navigation_container')
+      expect($component2.length).not.toBeTruthy()
+      expect(htmlWithClassName($, '.lbs-header')).toMatchSnapshot()
+    })
+
+    it('renders without the nav and without the search', () => {
+      const $ = render('header', examples['with no navigation or search'])
+
+      const $component = $('.lbs-header #lbs-header__search')
+      expect($component.length).not.toBeTruthy()
+      const $component2 = $('.lbs-header #lbs-header__navigation_container')
+      expect($component2.length).not.toBeTruthy()
+      expect(htmlWithClassName($, '.lbs-header')).toMatchSnapshot()
+    })
+  })
+})
