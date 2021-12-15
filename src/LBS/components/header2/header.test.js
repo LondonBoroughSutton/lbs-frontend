b49ded1574113f099e398a573185a0faa86e9e2a
benwatsonuk <hello@benwatson.eu>
2021-12-15 12:55:37 +0000

    Additional test coverage

--- src/LBS/components/header2/header.test.js
+++ src/LBS/components/header2/header.test.js
@@ -14,88 +14,118 @@ describe('/components/header', () => {
       afterEach(async () => {
         await page.evaluate(() => window.sessionStorage.clear())
       })
-      describe('the search function', () => {
-        it('clicking search button should give self \'active\' class', async () => {
-          await page.emulate(iPhone)
-          await page.goto(baseUrl + '/components/header/preview', { waitUntil: 'load' })
 
-          const initialState = await page.evaluate(() => document.body.querySelector('#lbs-header__mobile__search-btn').classList.contains('active'))
-          await page.click('#lbs-header__mobile__search-btn')
-          const newState = await page.evaluate(() => document.body.querySelector('#lbs-header__mobile__search-btn').classList.contains('active'))
-          expect(initialState).toBe(false)
-          expect(newState).toBe(true)
+      describe('on mobile devices', () => {
+
+        describe('the search function', () => {
+          // it('clicking search button should give self \'open\' class', async () => {
+          //   await page.emulate(iPhone)
+          //   await page.goto(baseUrl + '/components/header/preview', { waitUntil: 'load' })
+          //
+          //   const initialState = await page.evaluate(() => document.body.querySelector('button[aria-controls="super-search-menu"]').classList.contains('gem-c-layout-super-navigation-header__open-button'))
+          //   await page.click('button[aria-controls="super-search-menu"]')
+          //   const newState = await page.evaluate(() => document.body.querySelector('button[aria-controls="super-search-menu"]').classList.contains('gem-c-layout-super-navigation-header__open-button'))
+          //   expect(initialState).toBe(false)
+          //   expect(newState).toBe(true)
+          // })
+
+          // it('clicking search button should give \'lbs-header__search\' \'active\' class', async () => {
+          //   await page.emulate(iPhone)
+          //   await page.goto(baseUrl + '/components/header/preview', { waitUntil: 'load' })
+          //
+          //   const initialState = await page.evaluate(() => document.body.querySelector('#lbs-header__search').classList.contains('active'))
+          //   await page.click('#lbs-header__mobile__search-btn')
+          //   const newState = await page.evaluate(() => document.body.querySelector('#lbs-header__search').classList.contains('active'))
+          //   expect(initialState).toBe(false)
+          //   expect(newState).toBe(true)
+          // })
+
         })
 
-        it('clicking search button should give \'lbs-header__search\' \'active\' class', async () => {
-          await page.emulate(iPhone)
-          await page.goto(baseUrl + '/components/header/preview', { waitUntil: 'load' })
+        describe('the mobile nav function', () => {
+          it('clicking mobile nav button should give self \'active\' class', async () => {
+            await page.emulate(iPhone)
+            await page.goto(baseUrl + '/components/header/preview', { waitUntil: 'load' })
 
-          const initialState = await page.evaluate(() => document.body.querySelector('#lbs-header__search').classList.contains('active'))
-          await page.click('#lbs-header__mobile__search-btn')
-          const newState = await page.evaluate(() => document.body.querySelector('#lbs-header__search').classList.contains('active'))
-          expect(initialState).toBe(false)
-          expect(newState).toBe(true)
+            const initialState = await page.evaluate(() => document.body.querySelector('#lbs-header__mobile__menu-btn').classList.contains('active'))
+            await page.click('#lbs-header__mobile__menu-btn')
+            const newState = await page.evaluate(() => document.body.querySelector('#lbs-header__mobile__menu-btn').classList.contains('active'))
+            expect(initialState).toBe(false)
+            expect(newState).toBe(true)
+          })
+
+          it('clicking mobile nav button should give \'lbs-header__navigation_container\' \'active\' class', async () => {
+            await page.emulate(iPhone)
+            await page.goto(baseUrl + '/components/header/preview', { waitUntil: 'load' })
+
+            const initialState = await page.evaluate(() => document.body.querySelector('#lbs-header__navigation_container').classList.contains('active'))
+            await page.click('#lbs-header__mobile__menu-btn')
+            const newState = await page.evaluate(() => document.body.querySelector('#lbs-header__navigation_container').classList.contains('active'))
+            expect(initialState).toBe(false)
+            expect(newState).toBe(true)
+          })
+
+          it('clicking search button should move focus to search input', async () => {
+            await page.emulate(iPhone)
+            await page.goto(baseUrl + '/components/header/preview', { waitUntil: 'load' })
+
+            const initialState = await page.evaluateHandle(() => document.activeElement)
+            await page.click('#lbs-header__mobile__search-btn')
+            const newState = await page.evaluateHandle(() => document.activeElement)
+            expect(initialState._remoteObject.className).toBe('HTMLBodyElement')
+            expect(newState._remoteObject.className).toBe('HTMLInputElement')
+          })
         })
 
-        it('clicking search button should move focus to search input', async () => {
-          await page.emulate(iPhone)
-          await page.goto(baseUrl + '/components/header/preview', { waitUntil: 'load' })
-
-          const initialState = await page.evaluateHandle(() => document.activeElement)
-          await page.click('#lbs-header__mobile__search-btn')
-          const newState = await page.evaluateHandle(() => document.activeElement)
-          expect(initialState._remoteObject.className).toBe('HTMLBodyElement')
-          expect(newState._remoteObject.className).toBe('HTMLInputElement')
-        })
       })
 
-      describe('the mobile nav function', () => {
-        it('clicking mobile nav button should give self \'active\' class', async () => {
-          await page.emulate(iPhone)
-          await page.goto(baseUrl + '/components/header/preview', { waitUntil: 'load' })
+      describe('on desktop devices', () => {
 
-          const initialState = await page.evaluate(() => document.body.querySelector('#lbs-header__mobile__menu-btn').classList.contains('active'))
-          await page.click('#lbs-header__mobile__menu-btn')
-          const newState = await page.evaluate(() => document.body.querySelector('#lbs-header__mobile__menu-btn').classList.contains('active'))
-          expect(initialState).toBe(false)
-          expect(newState).toBe(true)
+        // describe('the search function', () => {
+        //   it('clicking search button should give self \'open\' class', async () => {
+        //     await page.emulate(iPhone)
+        //     await page.goto(baseUrl + '/components/header/preview', { waitUntil: 'load' })
+        //
+        //     const initialState = await page.evaluate(() => document.body.querySelector('button[aria-controls="super-search-menu"]').classList.contains('gem-c-layout-super-navigation-header__open-button'))
+        //     await page.click('button[aria-controls="super-search-menu"]')
+        //     const newState = await page.evaluate(() => document.body.querySelector('button[aria-controls="super-search-menu"]').classList.contains('gem-c-layout-super-navigation-header__open-button'))
+        //     expect(initialState).toBe(false)
+        //     expect(newState).toBe(true)
+        //   })
+
         })
 
-        it('clicking mobile nav button should give \'lbs-header__navigation_container\' \'active\' class', async () => {
-          await page.emulate(iPhone)
-          await page.goto(baseUrl + '/components/header/preview', { waitUntil: 'load' })
+      describe('on all devices', () => {
 
-          const initialState = await page.evaluate(() => document.body.querySelector('#lbs-header__navigation_container').classList.contains('active'))
-          await page.click('#lbs-header__mobile__menu-btn')
-          const newState = await page.evaluate(() => document.body.querySelector('#lbs-header__navigation_container').classList.contains('active'))
-          expect(initialState).toBe(false)
-          expect(newState).toBe(true)
+        describe('the search function', () => {
+          it('clicking search button should give self \'open\' class', async () => {
+            await page.goto(baseUrl + '/components/header/preview', { waitUntil: 'load' })
+
+            const initialState = await page.evaluate(() => document.body.querySelector('button[aria-controls="super-search-menu"]').classList.contains('gem-c-layout-super-navigation-header__open-button'))
+            await page.click('button[aria-controls="super-search-menu"]')
+            const newState = await page.evaluate(() => document.body.querySelector('button[aria-controls="super-search-menu"]').classList.contains('gem-c-layout-super-navigation-header__open-button'))
+            expect(initialState).toBe(false)
+            expect(newState).toBe(true)
+          })
+
+          it('clicking search button should move focus to search input', async () => {
+            await page.emulate(iPhone)
+            await page.goto(baseUrl + '/components/header/preview', { waitUntil: 'load' })
+
+            const initialState = await page.evaluateHandle(() => document.activeElement)
+            await page.click('button[aria-controls="super-search-menu"]')
+            const newState = await page.evaluateHandle(() => document.activeElement)
+            expect(initialState._remoteObject.className).toBe('HTMLBodyElement')
+            expect(newState._remoteObject.className).toBe('HTMLInputElement')
+          })
         })
 
-        it('clicking search button should move focus to search input', async () => {
-          await page.emulate(iPhone)
-          await page.goto(baseUrl + '/components/header/preview', { waitUntil: 'load' })
+        describe('the navigation function', () => {
 
-          const initialState = await page.evaluateHandle(() => document.activeElement)
-          await page.click('#lbs-header__mobile__search-btn')
-          const newState = await page.evaluateHandle(() => document.activeElement)
-          expect(initialState._remoteObject.className).toBe('HTMLBodyElement')
-          expect(newState._remoteObject.className).toBe('HTMLInputElement')
         })
+
       })
 
-      describe('the main navigation menu', () => {
-        it('interacting with a on level 1 nav item alters aria-expanded value', async () => {
-          await page.emulate(puppeteer.devices['iPad landscape'])
-          await page.goto(baseUrl + '/components/header/preview', { waitUntil: 'load' })
-
-          const initialState = await page.evaluate(() => document.body.querySelector('#lbs-header__navigation a:first-of-type').getAttribute('aria-expanded'))
-          await page.click('.lbs-header__navigation__item--level1')
-          const newState = await page.evaluate(() => document.body.querySelector('#lbs-header__navigation a:first-of-type').getAttribute('aria-expanded'))
-          expect(initialState).toBe('false')
-          expect(newState).toBe('true')
-        })
-      })
     })
 
     describe('when JavaScript is not available', () => {
