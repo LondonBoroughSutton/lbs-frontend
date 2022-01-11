ca650539e7174fd5134e4a4b9a74009f1951bee2
benwatsonuk <hello@benwatson.eu>
2022-01-11 11:55:55 +0000

    Bumped version

--- package/LBS/components/card/card.js
+++ package/LBS/components/card/card.js
@@ -174,7 +174,11 @@ Cards.prototype.setupCardWrapper = function () {
 Cards.prototype.setupResponsiveChecks = function () {
   this.mql = window.matchMedia('(min-width: ' + settings.minWidth + ')');
   this.mql.addListener(this.checkMode.bind(this));
+  const that = this;
   this.checkMode();
+  window.onresize = function () {
+    that.checkMode();
+  };
 };
 
 Cards.prototype.checkMode = function () {
@@ -196,6 +200,7 @@ Cards.prototype.setHeight = function () {
   // todo - consider adding parameter to ignore certain items (opt in)
   let tallestCard = 0;
   this.$module.querySelectorAll('.lbs-card').forEach(card => {
+    card.style.minHeight = '0';
     if (card.clientHeight > tallestCard) {
       const cs = window.getComputedStyle(card);
       tallestCard = card.clientHeight - (parseFloat(cs.paddingBottom));
