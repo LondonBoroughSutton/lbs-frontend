3a59a3108f79f46d879efdd77ff9392611d506a0
benwatsonuk <hello@benwatson.eu>
2022-01-12 09:16:14 +0000

    Added the close form button and functionality

--- src/LBS/components/page-feedback/page-feedback.js
+++ src/LBS/components/page-feedback/page-feedback.js
@@ -3,6 +3,7 @@ function PageFeedback ($module) {
   this.jshiddenClass = 'js_is-hidden'
   this.jsShowClass = 'js_show'
   this.prompt = 'lbs-page-feedback__prompt'
+  this.formController = this.$module.querySelector('[aria-controls=' + this.feedbackForm + ']')
   this.feedbackForm = 'lbs-page-feedback__form'
   this.formToggleButton = 'lbs-page-feedback__form'
 }
@@ -21,6 +22,7 @@ PageFeedback.prototype.init = function () {
   this.$module.querySelector('[aria-controls=' + this.feedbackForm + ']').addEventListener('click', function (t) {
     t.preventDefault()
     this.toggleForm(t.target)
+    this.addHideButton()
   }.bind(this))
 }
 
@@ -29,14 +31,37 @@ PageFeedback.prototype.setInitialAriaAttributes = function () {
 }
 
 PageFeedback.prototype.updateAriaAttributes = function (t) {
-  t.setAttribute('aria-expanded', !0)
-  document.querySelector('#' + t.getAttribute('aria-controls')).setAttribute('aria-hidden', !1)
+    if (t.getAttribute('aria-expanded') === 1) {
+      t.setAttribute('aria-expanded', !0)
+      document.querySelector('#' + t.getAttribute('aria-controls')).setAttribute('aria-hidden', !1)
+    } else {
+      t.setAttribute('aria-expanded', !1)
+      document.querySelector('#' + t.getAttribute('aria-controls')).setAttribute('aria-hidden', !0)
+    }
 }
 
 PageFeedback.prototype.toggleForm = function (t) {
   this.$module.querySelector('.lbs-page-feedback__form').classList.toggle(this.jsShowClass)
   this.$module.querySelector('.lbs-page-feedback__prompt').classList.toggle(this.jshiddenClass)
   this.updateAriaAttributes(t)
+}
+
+PageFeedback.prototype.addHideButton = function (t) {
+  const closeForm = document.createElement('a')
+  const module = this.$module
+  const that = this
+  closeForm.innerText = 'Close'
+  closeForm.setAttribute('class', 'lbs-button--secondary lbs-button govuk-button')
+  closeForm.setAttribute('id', 'lbs-feedback__form--hide-button')
+  closeForm.setAttribute('href', '#')
+  closeForm.setAttribute('aria-controls', this.feedbackForm)
+  closeForm.setAttribute('role', 'button')
+  closeForm.addEventListener('click', function (e) {
+    e.preventDefault()
+    module.querySelector('#lbs-feedback__form--hide-button').remove()
+    that.toggleForm(e.target)
+  })
+  this.$module.querySelector('form').prepend(closeForm)
 }
 
 export default PageFeedback
